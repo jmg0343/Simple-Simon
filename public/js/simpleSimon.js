c171a10cc6816b362d5b4e35df874999605d4dce
Joey Garcia <jmg0343@gmail.com>
2016-08-18 15:12:54 -0500

    Added a test button to ensure that randomly generated numbers were being added to array Colored buttons now light up according to randomly generated number as test button is pushed

--- public/js/simpleSimon.js
+++ public/js/simpleSimon.js
@@ -3,50 +3,91 @@ $(document).ready(function () {
 
 // ====================== Randomize ========================================
 
-	var random = Math.floor(Math.random() * 4) + 1; 
- 	console.log(random);
-// // =========================================================================
+	// var random = Math.floor(Math.random() * 4) + 1; 
+ // 	console.log(random);
 
+// // ======================================================================
+
+
+
+// push random numbers into array
+	var random;
+	var randomArray = [];
+	// randomArray.push(random);
+
+	$("#test").click(function(){
+		random = Math.floor(Math.random() * 4) + 1;
+		randomArray.push(random);
+		console.log(randomArray);
+
+		switch (random) {
+			case 1:
+				$("#green").fadeTo("slow", 1, function(){
+					$("#green").fadeTo(function(){
+						$("#green").css("opacity", ".5");
+					});
+				});
+				break;
+			case 2:
+				$("#red").fadeTo("slow", 1, function(){
+					$("#red").fadeTo(function(){
+						$("#red").css("opacity", ".5");
+					});
+				});
+				break;
+			case 3:
+				$("#yellow").fadeTo("slow", 1, function(){
+					$("#yellow").fadeTo(function(){
+						$("#yellow").css("opacity", ".5");
+					});
+				});
+				break;
+			case 4:
+				$("#blue").fadeTo("slow", 1, function(){
+					$("#blue").fadeTo(function(){
+						$("#blue").css("opacity", ".5");
+					});
+				});
+				break;
+		};
+	});
+
+
+
+// ================== Random Button Lights Up ==========================================
 // switch statement. cases are numbers associated with colors, which will light up. 
 
-	switch (random) {
-		case 1:
-			$("#green").fadeTo("slow", 1, function(){
-				$("#green").fadeTo(function(){
-					$("#green").css("opacity", ".5");
-				});
-			});;
-			break;
-		case 2:
-			$("#red").fadeTo("slow", 1, function(){
-				$("#red").fadeTo(function(){
-					$("#red").css("opacity", ".5");
-				});
-			});;
-			break;
-		case 3:
-			$("#yellow").fadeTo("slow", 1, function(){
-				$("#yellow").fadeTo(function(){
-					$("#yellow").css("opacity", ".5");
-				});
-			});;
-			break;
-		case 4:
-			$("#blue").fadeTo("slow", 1, function(){
-				$("#blue").fadeTo(function(){
-					$("#blue").css("opacity", ".5");
-				});
-			});;
-			break;
-	}
+	// switch (random) {
+	// 	case 1:
+	// 		$("#green").fadeTo("slow", 1, function(){
+	// 			$("#green").fadeTo(function(){
+	// 				$("#green").css("opacity", ".5");
+	// 			});
+	// 		});
+	// 		break;
+	// 	case 2:
+	// 		$("#red").fadeTo("slow", 1, function(){
+	// 			$("#red").fadeTo(function(){
+	// 				$("#red").css("opacity", ".5");
+	// 			});
+	// 		});
+	// 		break;
+	// 	case 3:
+	// 		$("#yellow").fadeTo("slow", 1, function(){
+	// 			$("#yellow").fadeTo(function(){
+	// 				$("#yellow").css("opacity", ".5");
+	// 			});
+	// 		});
+	// 		break;
+	// 	case 4:
+	// 		$("#blue").fadeTo("slow", 1, function(){
+	// 			$("#blue").fadeTo(function(){
+	// 				$("#blue").css("opacity", ".5");
+	// 			});
+	// 		});
+	// 		break;
+	// }
 
-// ======================== variables representing colored buttons established and pushed into array =========================
-	// var green = $("#green");
-	// var red = $("#red");
-	// var yellow = $("#yellow");
-	// var blue = $("#blue");
-	// var colorArray = [green, red, yellow, blue];	
-	// console.log(colorArray);
 
 // ========================= flashes button =======================
 	
